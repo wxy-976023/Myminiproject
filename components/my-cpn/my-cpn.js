9e8596e171f13f434e51039f68fd2484e5970b32
wxy-976023 <w19800322592@163.com>
2020-12-04 09:20:27 +0800

    组件化开发a

---
+++ components/my-cpn/my-cpn.js
@@ -0,0 +1,33 @@
+// components/my-cpn/my-cpn.js
+Component({
+  /**
+  options: {
+    样式相互隔离
+    styleIsolation:"isolated",
+    页面影响自定义组件
+    styleIsolation:"apply-shared",
+    组件影响页面
+    styleIsolation:"shared"
+  },
+   */
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/my-prop/my-prop.js
@@ -0,0 +1,31 @@
+// components/my-prop/my-prop.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:"我是默认内容"
+    },
+    content:{
+      type:Number,
+      value:"00000000"
+    }
+  },
+  externalClasses: ['titleclass'],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
